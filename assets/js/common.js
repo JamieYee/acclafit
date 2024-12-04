a152ec9ed55a5a1dc9e907bf0c70305e4f1103bc
yishangfei <yishangfei.me@gmail.com>
2024-12-04 21:26:03 +0800

    优化代码

--- assets/js/common.js
+++ assets/js/common.js
@@ -22,9 +22,9 @@ async function setCommon() {
 
     // 定义新的联系方式
     const newContactInfo = {
-        currency: "£",
-        band: "Acclafit",
-        bandSite: "https://acclafit.com/",
+        currency: "$",
+        band: "Asmoda",
+        bandSite: "https://asmoda.store/",
         phone: "(+44) 7496 274719",
         email: "amazonbeboss@gmail.com",
         address: "Office 101 32 Threadneedle Street, London, United Kingdom, EC2R 8AY"
