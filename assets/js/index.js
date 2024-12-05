62f5fd1c8e5c30e2e10ca5cebc12d86e876e521d
yishangfei <yishangfei.me@gmail.com>
2024-12-05 20:31:33 +0800

    修复问题

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(products => {
       //修改最低价格
       const lowestPrice = Math.min(...products.map(product => product.price));
-      document.getElementById('price-value').textContent = '$' + lowestPrice; // 更新价格
+      document.querySelectorAll('.price-value').forEach(element => {
+            element.textContent = '$' + lowestPrice;
+        });
+      // document.getElementById('price-value').textContent = '$' + lowestPrice; // 更新价格
       //获取所有.response - product 容器
       const containers = document.querySelectorAll('.response-product');
       // 遍历每个容器
