acc7d0382a363c9628d52fb6fa549f53104b7b26
yishangfei <yishangfei.me@gmail.com>
2025-02-21 21:06:07 +0800

    lirrope

--- assets/js/common.js
+++ assets/js/common.js
@@ -25,9 +25,9 @@ async function setCommon() {
 
     // 定义新的联系方式
     const newContactInfo = {
-        currency: "£",
-        band: "ZPPSNFIT",
-        bandSite: "https://www.zppsnfit.com",
+        currency: "$",
+        band: "Liretope",
+        bandSite: "https://www.Liretope.com",
         phone: "(+44) 7496 274719",
         email: "amazonbeboss@gmail.com",
         address: "Office 101 32 Threadneedle Street, London, United Kingdom, EC2R 8AY"
